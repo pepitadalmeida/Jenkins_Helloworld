20bfee6f2a02e644ce5f99535e852ea24e1d1774
pepitadalmeida <153777884+pepitadalmeida@users.noreply.github.com>
2025-02-04 14:33:16 +0100

    Update Main.java
    ok

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Pépita D'ALMEIDA!");
+        System.out.println("Hello, Pépita Josephine D'ALMEIDA!");
     }
 }
 
